88eaea47f09baeefce6eb5e91d2298fadea1401a
digovc <rodrigo.conspena@gmail.com>
2025-02-28 13:11:54 -0300

    Add .env file for API URL configuration and refactor API methods to use axios client

--- src/api/api.base.js
+++ src/api/api.base.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export class ApiBase {
   constructor() {
-    this.api = axios.create({
+    this.client = axios.create({
       baseURL: import.meta.env.VITE_API_URL
     });
   }
--- src/api/projects.api.js
+++ src/api/projects.api.js
@@ -7,23 +7,23 @@ export class ProjectsApi extends ApiBase {
   }
 
   async getAllProjects() {
-    return this.get(this.baseUrl);
+    return this.client.get(this.baseUrl);
   }
 
   async getProjectById(id) {
-    return this.get(`${this.baseUrl}/${id}`);
+    return this.client.get(`${this.baseUrl}/${id}`);
   }
 
   async createProject(projectData) {
-    return this.post(this.baseUrl, projectData);
+    return this.client.post(this.baseUrl, projectData);
   }
 
   async updateProject(id, projectData) {
-    return this.put(`${this.baseUrl}/${id}`, projectData);
+    return this.client.put(`${this.baseUrl}/${id}`, projectData);
   }
 
   async deleteProject(id) {
-    return this.delete(`${this.baseUrl}/${id}`);
+    return this.client.delete(`${this.baseUrl}/${id}`);
   }
 }
 
