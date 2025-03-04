f102743ad80f2794c1963b38028ed9add3c6560a
digovc <rodrigo.conspena@gmail.com>
2025-03-04 13:05:41 -0300

    Refactor ChatComponent to improve layout and remove unnecessary comments

--- src/pages/project/ChatComponent.vue
+++ src/pages/project/ChatComponent.vue
@@ -2,19 +2,16 @@
   <div class="bg-gray-900 rounded-lg shadow-md p-6 h-full flex flex-col">
     <h2 class="text-2xl font-bold mb-4">Chat</h2>
     <div class="flex flex-col h-full">
-      <!-- Área de conversação (topo, com espaço flexível) -->
       <div class="flex-grow overflow-y-auto mb-4 relative">
         <div class="absolute inset-0 overflow-y-auto">
           <ChatConversationComponent :conversation="selectedConversation"/>
         </div>
       </div>
 
-      <!-- Área de ações do chat (meio) -->
       <div class="mb-3">
         <ChatActionsComponent :conversations="conversations" :selectedConversation="selectedConversation" :onSelectConversation="selectConversation"/>
       </div>
 
-      <!-- Área de input (parte inferior) -->
       <div>
         <ChatInputComponent :onSendMessage="sendMessage"/>
       </div>
