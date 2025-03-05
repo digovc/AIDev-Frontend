c6454b107fb5e8fad1ff43d7038883226128d57b
digovc <rodrigo.conspena@gmail.com>
2025-03-05 12:10:46 -0300

    Refactor TaskComponent to remove 'play now' button and streamline task control options

--- src/pages/project/taks/TaskComponent.vue
+++ src/pages/project/taks/TaskComponent.vue
@@ -10,9 +10,6 @@
       <button v-if="task.status !== 'running'" @click="$emit('play', task.id)" class="text-green-400 hover:text-green-300" title="Iniciar">
         <FontAwesomeIcon :icon="faPlay"/>
       </button>
-      <button v-if="task.status !== 'running'" @click="$emit('play-now', task.id)" class="text-yellow-400 hover:text-yellow-300" title="Iniciar agora (prioridade)">
-        <FontAwesomeIcon :icon="faBoltLightning"/>
-      </button>
       <button v-if="task.status === 'running'" @click="$emit('stop', task.id)" class="text-red-400 hover:text-red-300" title="Parar">
         <FontAwesomeIcon :icon="faStop"/>
       </button>
@@ -23,7 +20,7 @@
 
 <script setup>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faBoltLightning, faCog, faPlay, faStop } from "@fortawesome/free-solid-svg-icons";
+import { faCog, faPlay, faStop } from "@fortawesome/free-solid-svg-icons";
 
 defineProps({
   task: {
