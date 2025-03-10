7744303ae0038bc0f16f4ea60277877da5595408
digovc <rodrigo.conspena@gmail.com>
2025-03-10 16:46:51 -0300

    Update AssistantsComponent layout for improved responsiveness

--- src/pages/home/assistants/AssistantsComponent.vue
+++ src/pages/home/assistants/AssistantsComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-4">
+  <div class="container mx-auto p-4">
     <h1 class="text-2xl font-bold mb-4">Assistentes</h1>
     <p>Página de assistentes em desenvolvimento.</p>
   </div>
